48bccc4081f55542f63e6383975d05d2b8be0326
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-24 00:24:38 +0300

    For checking on remote machine

---
+++ src/test/java/ru/netology/TestCardDelivery.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestCardDelivery {
+
+    @Test
+    void shouldCheckOrderCardWithDelivery() {
+//        Configuration.headless = true;
+        open("http://localhost:9999");
+        $("[data-test-id='city']").$("[type='text']").setValue("Самара");
+        // Вместо setValue использовал getValue чтобы по умолчанию установить ближайшую доступную дату и тест не
+        // пришлось бы править каждый раз как при использовании setValue
+        $$("[type='tel']").first().getCssValue("value");
+        $("[data-test-id='name']").$("[type='text']").setValue("Андреев Андрей");
+        $$("[type='tel']").last().setValue("+79998885577");
+        $("[data-test-id='agreement']").click();
+        $$("[type='button']").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).waitUntil(visible, 12000);
+    }
+}
