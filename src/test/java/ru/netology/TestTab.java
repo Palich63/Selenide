431954635adcb4d8f006d9eafe1a6ac61b9f7273
Andrey Globa <Globa.Andrey@gmail.com>
2020-07-25 21:51:24 +0300

    ready

--- src/test/java/ru/netology/TestTab.java
+++ src/test/java/ru/netology/TestTab.java
@@ -3,12 +3,10 @@ package ru.netology;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class TestTab {
 
